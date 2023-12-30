77a9c76990b3fc2144bd2b23ffc0f8d6d21d7d71
mirkosms <miroslawsmoronski+git@gmail.com>
2023-12-30 14:55:10 +0100

    Add Proper Display of Polish Characters in Facts
    - Added proper display of Polish characters in facts.
    - Converted Facts.cpp from ANSI to UTF-8.

--- KidsEduGame/src/Facts.cpp
+++ KidsEduGame/src/Facts.cpp
@@ -5,12 +5,12 @@
 
 Facts::Facts() {
     generalFacts = {
-        "S≥owo 'hundred' pochodzi od staronordyjskiego s≥owa 'hundrath', ktÛre oznacza 120, a nie 100.",
+        "S≈Çowo 'hundred' pochodzi od staronordyjskiego s≈Çowa 'hundrath', kt√≥re oznacza 120, a nie 100.",
         "Pierwiastek kwadratowy z 2 (1.41) jest znany jako stala Pitagorasa.",
         "Zero jest jedyna liczba, ktora nie moze byc reprezentowana w systemie rzymskim."
     };
     romanNumeralFacts = {
-        "Historia liczb rzymskich: Liczby rzymskie pochodzπ z staroøytnego Rzymu i by≥y powszechnie uøywane do zapisywania liczb w Europie aø do pÛünego úredniowiecza.",
+        "Historia liczb rzymskich: Liczby rzymskie pochodzƒÖ z staro≈ºytnego Rzymu i by≈Çy powszechnie u≈ºywane do zapisywania liczb w Europie a≈º do p√≥≈∫nego ≈õredniowiecza.",
         "Uzycie liczb rzymskich trwalo dlugo po upadku Cesarstwa Rzymskiego. Od XIV wieku liczby rzymskie zaczely byc zastepowane przez liczby arabskie.",
         "Liczby rzymskie sa nadal uzywane w niektorych zastosowaniach do dnia dzisiejszego, na przyklad na tarczach zegarow."
     };
@@ -24,9 +24,9 @@ std::string Facts::getRandomGeneralFact() {
 
     std::size_t index;
     for (index = dist(rng); generalFactsDisplayed[index]; index = dist(rng)) {
-        // Sprawdzamy, czy wszystkie ciekawostki zosta≥y wyúwietlone
+        // Sprawdzamy, czy wszystkie ciekawostki zosta≈Çy wy≈õwietlone
         if (std::all_of(generalFactsDisplayed.begin(), generalFactsDisplayed.end(), [](bool v) { return v; })) {
-            std::fill(generalFactsDisplayed.begin(), generalFactsDisplayed.end(), false); // Resetuj stan wyúwietlania
+            std::fill(generalFactsDisplayed.begin(), generalFactsDisplayed.end(), false); // Resetuj stan wy≈õwietlania
         }
     }
     generalFactsDisplayed[index] = true;
@@ -39,10 +39,10 @@ std::string Facts::getRandomRomanNumeralFact() {
 
     std::size_t index;
     for (index = dist(rng); romanNumeralFactsDisplayed[index]; index = dist(rng)) {
-        // Sprawdzamy, czy wszystkie ciekawostki zosta≥y wyúwietlone
+        // Sprawdzamy, czy wszystkie ciekawostki zosta≈Çy wy≈õwietlone
         if (std::all_of(romanNumeralFactsDisplayed.begin(), romanNumeralFactsDisplayed.end(), [](bool v) { return v; })) {
-            std::fill(romanNumeralFactsDisplayed.begin(), romanNumeralFactsDisplayed.end(), false); // Resetuj stan wyúwietlania
-            break; // Opcjonalnie, moøemy wyjúÊ z pÍtli, jeúli wszystkie fakty zosta≥y zresetowane
+            std::fill(romanNumeralFactsDisplayed.begin(), romanNumeralFactsDisplayed.end(), false); // Resetuj stan wy≈õwietlania
+            break; // Opcjonalnie, mo≈ºemy wyj≈õƒá z pƒôtli, je≈õli wszystkie fakty zosta≈Çy zresetowane
         }
     }
     romanNumeralFactsDisplayed[index] = true;
@@ -57,7 +57,7 @@ bool Facts::allFactsDisplayed() const {
 }
 
 void Facts::resetFactsDisplayStatus() {
-    // Resetuje stan wyúwietlania dla wszystkich ciekawostek
+    // Resetuje stan wy≈õwietlania dla wszystkich ciekawostek
     std::fill(generalFactsDisplayed.begin(), generalFactsDisplayed.end(), false);
     std::fill(romanNumeralFactsDisplayed.begin(), romanNumeralFactsDisplayed.end(), false);
 }
